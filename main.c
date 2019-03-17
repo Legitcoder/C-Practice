250bcd6babf3ba3498825413dbe536fa14767f42
Moin Uddin <U_Moin75@yahoo.com>
2019-03-16 20:35:08 -0400

    Practice Strings, Characters, Scanf, Printf, Basic Math

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+    //Characters and Strings
+//    char myName[] = "Moin";
+//    strcpy(myName, "Dee");
+//    printf("My name is %s\n\n", myName);
+
+    //ScanF
+//    char lastInitial;
+//
+//    printf("What is your last initial? ");
+//    scanf(" %c", &lastInitial);
+//    printf("Last initial %c\n", lastInitial);
+//
+//    char firstName[30], lastName[30];
+//
+//    printf("What is your name? ");
+//
+//    scanf(" %s %s", firstName, lastName);
+//
+//    printf("Your name is %s %c %s\n\n", firstName, lastInitial, lastName);
+//
+//
+//    int month, day, year;
+//
+//    printf("What's your birth date? ");
+//
+//    scanf(" %d/%d/%d", &month, &day, &year);
+//
+//    printf("\nBirth Date %d/%d/%d\n\n", month, day, year);
+
+
+    //Math Operators
+
+//    int num1 = 12, num2 = 15, numAns;
+//    float decimal1 = 1.2, decimal2 = 1.5, decimalAns;
+//
+//    printf("Integer Calculation %d\n\n", num2 / num1);
+//
+//    printf("Float Calculation %f\n\n", decimal2 / decimal1);
+//
+//    printf("Modulus %d\n\n", num2 % num1);
+//
+//    printf("Without Parantheses %d\n\n", 3 + 6 * 10);
+//
+//    printf("With Parantheses %d\n\n", (3+6) * 10);
+
+
+//    int randomNum = 1;
+//
+//    printf("%d += 2 : %d\n\n", randomNum, randomNum += 2);
+//
+//    // %d is printed after the calculation here
+//    printf("%d += 2 : %d\n\n", randomNum, randomNum += 2);
+
+//    int exNum = 1;
+//
+//    printf("++%d : %d\n\n", exNum, ++exNum);
+//
+//    exNum = 1;
+//
+//    printf("%d++ : %d\n\n", exNum, exNum++);
+
+
+    int numberEx = 12;
+    int numberEx2 = 14;
+
+    printf("NumberEx / NumberEx2: %f\n\n", (float) numberEx / numberEx2);
+
+
+
+    return 0;
+}
