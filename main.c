75857408278c05444c8d6e2b4f046f1c9586c063
Moin Uddin <U_Moin75@yahoo.com>
2019-03-18 03:37:10 -0400

    More char manipulation

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 int globalVar = 0;
 
@@ -98,6 +100,55 @@ void setDogWeightPtr(dog *theDog, int newWeight)
     (*theDog).avgWeightLbs = newWeight;
 
     printf("The weight was changed to %d\n\n", (*theDog).avgWeightLbs);
+}
+
+void noMoreNewline(char *theString)
+{
+    char *isANewline;
+
+    isANewline = strrchr(theString, '\n');
+
+    if(isANewline){
+
+        *isANewline = '\0';
+    }
+}
+
+void makeLowercase(char *theString) {
+    int i = 0;
+
+    while(theString[i]) {
+
+        theString[i] = tolower(theString[i]);
+        i++;
+    }
+}
+
+void getCharInfo(){
+
+    char theChar;
+
+    while((theChar = getchar()) != '\n'){
+        printf("Letter or Number %d\n\n", isalnum(theChar));
+
+        printf("Alphabetic Char %d\n\n", isalpha(theChar));
+
+        printf("Standard Blank %d\n\n", isblank(theChar));
+
+        printf("Ctrl Char %d\n\n", iscntrl(theChar));
+
+        printf("Number Char %d\n\n", isdigit(theChar));
+
+        printf("Anything But Space %d\n\n", isgraph(theChar));
+
+        printf("Lowercase %d\n\n", islower(theChar));
+
+        printf("Uppercase %d\n\n", isupper(theChar));
+
+        printf("Punctuation %d\n\n", ispunct(theChar));
+
+        printf("Any Space %d\n\n", isspace(theChar));
+    }
 }
 
 int main() {
@@ -390,12 +441,97 @@ int main() {
 //    getMemoryLocations(cujo);
 //
 //    getMemoryLocations(cujo2);
+//
+//    dog benji = {"Benji", "Silk Terrier", 25, 9, {"Meat", "Joe Camp"}};
+//
+//    setDogWeightPtr(&benji, 11);
+//
+//    printf("Benji's weight in Main(): %d\n\n", benji.avgWeightLbs);
+//
+//
+//    //Boolean
+//    _Bool isANumber;
+//
+//    int number;
+//    int sumOfNumbers = 0;
+//
+//    printf("Enter a Number: ");
+//
+//    isANumber = (scanf("%d", &number) == 1);
+//
+//
+//    while(isANumber) {
+//        sumOfNumbers = sumOfNumbers + number;
+//        printf("Enter a Number: ");
+//
+//        isANumber = (scanf("%d", &number) == 1);
+//    }
+//
+//    printf("The Sum is %d\n\n", sumOfNumbers);
+//
+//    //Char manipulation
+//    char theChar;
+//
+//    while((theChar = getchar()) != '~'){
+//
+//        putchar(theChar);
+//    }
+//
+//    char name[50];
+//
+//    printf("What is your name? ");
+//
+//    gets(name);
+//
+//    puts("Hi");
+//    puts(name);
+//
+//    fgets(name, 50, stdin);
+//
+//    fputs("Hi ", stdout);
+//    fputs(name, stdout);
+//
+//    char *randomString = "Just some random stuff";
+//
+//    while(*randomString){
+//
+//        putchar(*randomString++);
+//    }
+//
+//    int i = 0;
+//
+//    while(randomString[i] != '\0') {
+//        putchar(*randomString++);
+//    }
+//
+//
+//    char doYouWantToQuit[10];
+//
+//    printf("Enter quit to quit: ");
+//
+//    fgets(doYouWantToQuit, 10, stdin);
+//
+//    noMoreNewline(doYouWantToQuit);
+//
+//    makeLowercase(doYouWantToQuit);
+//
+//    while(strcmp(doYouWantToQuit, "quit")) {
+//        printf("Enter quit to quit: ");
+//
+//        fgets(doYouWantToQuit, 10, stdin);
+//
+//        noMoreNewline(doYouWantToQuit);
+//
+//        makeLowercase(doYouWantToQuit);
+//
+//    }
+//
+//    printf("Thank you for typing quit %s\n\n", doYouWantToQuit);
 
-    dog benji = {"Benji", "Silk Terrier", 25, 9, {"Meat", "Joe Camp"}};
+//    getCharInfo();
 
-    setDogWeightPtr(&benji, 11);
 
-    printf("Benji's weight in Main(): %d\n\n", benji.avgWeightLbs);
+
 
     return 0;
 }
